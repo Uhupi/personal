9a90d5e18a36479fda846a1657870893a7cfb738
Uhupi <santino.lange@gmail.com>
2026-04-13 22:38:28 +0200

    Dist Build

--- vite.config.js
+++ vite.config.js
@@ -4,14 +4,13 @@ import vue from '@vitejs/plugin-vue'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [vue()],
-  publicDir: 'static',
   server: {
     watch: {
       usePolling: true,
     },
   },
   build: {
-    outDir: 'public',
-    emptyOutDir: false,
+    outDir: 'dist',
+    emptyOutDir: true,
   },
 })
